3a239ce1e9425ec326f510967f3326f5049b1b7c
David Cohen <cohendavidn@gmail.com>
2018-11-19 15:12:32 +0100

    Initial Commit

---
+++ Peerz/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Peerz
+//
+//  Created by David Cohen on 19/11/2018.
+//  Copyright © 2018 Peerz. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
