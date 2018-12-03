d0659053d3c8e1d470f4370be3952a587c7c719e
David Cohen <cohendavidn@gmail.com>
2018-12-03 11:42:51 +0100

    Start audio stream / fix bug for video

--- Peerz/View Controllers/FaceCallViewController.swift
+++ Peerz/View Controllers/FaceCallViewController.swift
@@ -11,7 +11,7 @@ import MultipeerConnectivity
 import AVFoundation
 import VideoToolbox
 
-class FaceCallViewController: UIViewController, MCSessionDelegate, MCBrowserViewControllerDelegate, AVCaptureVideoDataOutputSampleBufferDelegate {
+class FaceCallViewController: UIViewController, MCSessionDelegate, MCBrowserViewControllerDelegate, AVCaptureVideoDataOutputSampleBufferDelegate, AVAudioRecorderDelegate, AVCaptureAudioDataOutputSampleBufferDelegate {
     
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var videoView: UIView!
@@ -23,10 +23,21 @@ class FaceCallViewController: UIViewController, MCSessionDelegate, MCBrowserView
     var videoCaptureDevice : AVCaptureDevice?
     let sampleBufferQueue = DispatchQueue.global(qos: .userInteractive)
     
+    //Audio
+    var recordingSession = AVAudioSession()
+    var audioRecorder = AVAudioRecorder.self
+    var audioPlayer = AVAudioPlayer()
+    var settings = [String : Int]()
+    let audioSession = AVAudioSession.sharedInstance()
+    var audioOutput = AVCaptureAudioDataOutput()
+    //
+    
+    
     //Session
     var peerID: MCPeerID!
     var mcSession: MCSession!
     var mcAdvertiserAssistant: MCAdvertiserAssistant!
+    var stream: OutputStream!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -52,27 +63,86 @@ class FaceCallViewController: UIViewController, MCSessionDelegate, MCBrowserView
         captureSession.startRunning()
         
     }
-    
+
     //Video
     func setupCameraSession() {
         captureSession.beginConfiguration()
+        var defaultVideoDevice: AVCaptureDevice?
         
-//        let videoDevice = AVCaptureDevice.default(.builtInWideAngleCamera, for: .video, position: .front)
-//        guard
-//            let videoDeviceInput = try? AVCaptureDeviceInput(device: videoDevice!),
-//            captureSession.canAddInput(videoDeviceInput)
-//            else { return }
-//        captureSession.addInput(videoDeviceInput)
-
+        if let videoDevice = AVCaptureDevice.default(.builtInWideAngleCamera, for: .video, position: .front) {
+            defaultVideoDevice = videoDevice
+        }
+       
+        guard let videoDevice = defaultVideoDevice else {
+            print("Default video device is unavailable.")
+            captureSession.commitConfiguration()
+            return
+        }
+        
+        guard
+            let videoDeviceInput = try? AVCaptureDeviceInput(device: videoDevice),
+            captureSession.canAddInput(videoDeviceInput)
+            else { return }
+        captureSession.addInput(videoDeviceInput)
+        
         guard captureSession.canAddOutput(movieOutput) else { return }
         captureSession.sessionPreset = .medium
         captureSession.addOutput(movieOutput)
         captureSession.commitConfiguration()
+        previewLayer.videoGravity = AVLayerVideoGravity.resizeAspectFill
         movieOutput.setSampleBufferDelegate(self, queue: sampleBufferQueue)
+        //
+        
+        do {
+            let audioDevice = AVCaptureDevice.default(for: .audio)
+            let audioDeviceInput = try AVCaptureDeviceInput(device: audioDevice!)
+
+            if captureSession.canAddInput(audioDeviceInput) {
+                captureSession.addInput(audioDeviceInput)
+            } else {
+                print("Could not add audio device input to the session")
+            }
+        } catch {
+            print("Could not create audio device input: \(error)")
+        }
+
+        guard captureSession.canAddOutput(audioOutput) else { return }
+        captureSession.addOutput(audioOutput)
+        captureSession.commitConfiguration()
+        audioOutput.setSampleBufferDelegate(self, queue: sampleBufferQueue)
     }
     
     func captureOutput(_ output: AVCaptureOutput, didOutput sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection){
+        //Audio
+//        var blockBuffer: CMBlockBuffer!
+//        var audioBufferList = AudioBufferList()
+//
+//        CMSampleBufferGetAudioBufferListWithRetainedBlockBuffer(sampleBuffer, bufferListSizeNeededOut: nil, bufferListOut: &audioBufferList, bufferListSize: MemoryLayout.size(ofValue: AudioBufferList.self), blockBufferAllocator: nil, blockBufferMemoryAllocator: nil, flags: kCMSampleBufferFlag_AudioBufferList_Assure16ByteAlignment, blockBufferOut: &blockBuffer);
         
+        var audioBufferList = AudioBufferList(mNumberBuffers: 1, mBuffers: AudioBuffer(mNumberChannels: 0, mDataByteSize: 0, mData: nil))
+        var blockBuffer: CMBlockBuffer!
+        
+        CMSampleBufferGetAudioBufferListWithRetainedBlockBuffer(
+            sampleBuffer,
+            bufferListSizeNeededOut: nil,
+            bufferListOut: &audioBufferList,
+            bufferListSize: MemoryLayout.size(ofValue: audioBufferList),
+            blockBufferAllocator: nil,
+            blockBufferMemoryAllocator: nil,
+            flags: UInt32(kCMSampleBufferFlag_AudioBufferList_Assure16ByteAlignment),
+            blockBufferOut: &blockBuffer
+        )
+        
+        let audioBuffers = UnsafeBufferPointer<AudioBuffer>(start: &audioBufferList.mBuffers,
+                                                           count: Int(audioBufferList.mNumberBuffers))
+        
+        for audioBuffer in audioBuffers {
+//            if let x = audioBuffer.mData?.load(as: UnsafePointer<UInt8>.self) {
+//                stream.write(x, maxLength: Int(audioBuffer.mDataByteSize))
+//            }
+        }
+
+        //Video
         let imageBuffer = CMSampleBufferGetImageBuffer(sampleBuffer)
       
         guard let imgBuf = imageBuffer else { return }
@@ -119,6 +189,11 @@ class FaceCallViewController: UIViewController, MCSessionDelegate, MCBrowserView
     func disconnectSession(action: UIAlertAction!) {
         mcSession.disconnect()
     }
+    @IBAction func endCallButtonAction(_ sender: Any) {
+        mcSession.disconnect()
+        captureSession.stopRunning()
+        navigationController?.popViewController(animated: true)
+    }
     
     //Session
     func session(_ session: MCSession, peer peerID: MCPeerID, didChange state: MCSessionState) {
@@ -143,7 +218,8 @@ class FaceCallViewController: UIViewController, MCSessionDelegate, MCBrowserView
     }
     
     func session(_ session: MCSession, didReceive stream: InputStream, withName streamName: String, fromPeer peerID: MCPeerID) {
-        
+        stream.open()
+        print(stream)
     }
     
     func session(_ session: MCSession, didStartReceivingResourceWithName resourceName: String, fromPeer peerID: MCPeerID, with progress: Progress) {
@@ -155,12 +231,11 @@ class FaceCallViewController: UIViewController, MCSessionDelegate, MCBrowserView
     }
     
     func browserViewControllerDidFinish(_ browserViewController: MCBrowserViewController) {
+        self.stream = try?  mcSession.startStream(withName: "Audio", toPeer: mcSession.connectedPeers.first!)
         dismiss(animated: true)
     }
     
     func browserViewControllerWasCancelled(_ browserViewController: MCBrowserViewController) {
         dismiss(animated: true)
     }
-    
-
 }
